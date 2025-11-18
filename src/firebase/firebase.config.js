f247e1c6e21e23ce81790ce7d0fc34dd3dea0ffd
ShaheD922 <shahedhasan.yc@gmail.com>
2025-11-19 00:50:41 +0600

    complete

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration (from .env file)
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
